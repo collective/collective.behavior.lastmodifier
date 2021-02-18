bd826cd8c2da6e5786eb01fca3f9997a0cd53ac9
ale-rt <alessandro.pisa@gmail.com>
2021-02-18 17:34:32 +0100

    Preparing release 1.0.0rc1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.behavior.lastmodifier",
-    version="0.0.1a1",
+    version="1.0.0rc1",
     description="Adds a behavior that tracks down the last user that modified an object",  # noqa: E501
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
